ae27cd2f900c1c8b72a9d0b6fb1b7b64fed89158
Usheninte <jrandangana@gmail.com>
2019-03-31 16:14:32 +0100

    added Header

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import Header from './views/Header';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <p>Blanked</p>
+        <Header />
       </div>
     );
   }
